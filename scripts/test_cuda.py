5fe9317e6a67b333ab6f506d7cf7832ebfa20bdb
The Sang Nguyen <77398269+thesang-nguyen@users.noreply.github.com>
2023-06-27 18:18:55 +0200

    update test for cuda availability

--- scripts/test_cuda.py
+++ scripts/test_cuda.py
@@ -1,9 +1,19 @@
+import os
+import sys
 import torch
 
-if __name__ == '__main__':
-    print('torch version: ', torch.__version__)
-    print('torch file:    ', torch.__file__)
-    print('cuda available:', torch.cuda.is_available())
+
+def check():
+    print(f'which python3:   {sys.executable}')
+    print(f'torch version:   {torch.__version__}')
+    print(f'torch file:      {torch.__file__}')
+    print(f'cuda available:  {torch.cuda.is_available()}')
     if torch.cuda.is_available():
         for i in range(torch.cuda.device_count()):
-            print('device name:   ', torch.cuda.get_device_name(0))
+            print('device name:    ', torch.cuda.get_device_name(i))
+    print(f'LD_LIBRARY_PATH: {os.environ["LD_LIBRARY_PATH"]}\n')
+    print('')
+
+
+if __name__ == '__main__':
+    check()

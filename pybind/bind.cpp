3ff29f3793f37edf8801bc3a27b60ab6d11412a7
The Sang Nguyen <77398269+thesang-nguyen@users.noreply.github.com>
2023-06-28 14:08:12 +0200

    added inner newton kernel for unbalanced domdec with KL

--- pybind/bind.cpp
+++ pybind/bind.cpp
@@ -9,4 +9,5 @@ namespace py = pybind11;
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
   m.def("LogSumExpCUDA", &LogSumExpCUDA); 
   //m.def("LogSumExpGPU_accesor", &LogSumExpGPU_accesor);
+  m.def("InnerNewtonCUDA", &InnerNewtonCUDA);
 }
--- src/logsumexp.cu
+++ src/logsumexp.cu
@@ -48,3 +48,57 @@ void LogSumExpCUDAKernel(int B, int M, int N, float *alpha, float *beta, float d
     throw std::runtime_error(Formatter()
                              << "CUDA kernel failed : " << std::to_string(err));
 }
+
+//////////////////////////////////////////////////////////////////////
+// inner newton method kernel for unbalanced domdec with KL divergence
+//////////////////////////////////////////////////////////////////////
+
+__global__ void inner_newton(
+    int n_iter, float tol, int N, float *t, float eps, float lam,
+    float *lognu, float *lognu_nJ, float *logKTu
+) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= N) {
+        return;
+    }
+
+    float lameps = lam/eps;
+    float epslam = eps/lam;
+    float vmin, vmax, lse, g, g_prime; 
+    float err = tol + 1.0f;
+
+    int i = 0;
+    while (i < n_iter && err > tol) {
+        i++;
+        // logsumexp for 2 values
+        vmin = min(lognu_nJ[idx], -lameps*t[idx]);
+        vmax = max(lognu_nJ[idx], -lameps*t[idx]);
+        lse = vmax + log(exp(vmin - vmax) + 1.0f);
+
+        // compute g(t[idx]) and g'(t[idx])
+        g = lse - t[idx] - lognu[idx] - epslam*logKTu[idx];
+        g_prime = -lameps/(1 + exp(lameps*t[idx] + lognu_nJ[idx])) - 1;
+        
+        // update t
+        t[idx] = t[idx] - g/g_prime;
+        err = g;
+    }
+}
+
+void InnerNewtonCUDAKernel(
+    int n_iter, float tol, int N, float *t, float eps, float lam,
+    float *lognu, float *lognu_nJ, float *logKTu
+) {
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    inner_newton<<<numBlocks, blockSize>>>(
+        n_iter, tol, N, t, eps, lam, 
+        lognu, lognu_nJ, logKTu
+    );
+    cudaDeviceSynchronize();
+    cudaError_t err = cudaGetLastError();
+    if (cudaSuccess != err)
+    throw std::runtime_error(
+        Formatter() << "CUDA kernel failed : " << std::to_string(err)
+    );
+}

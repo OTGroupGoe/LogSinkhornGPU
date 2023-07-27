ad7d095fa2c542e40e6799d1e291fe1b105dbc9f
The Sang Nguyen <77398269+thesang-nguyen@users.noreply.github.com>
2023-07-27 22:17:14 +0200

    use log1p instead of log(1.0f + ...)

--- src/logsumexp.cu
+++ src/logsumexp.cu
@@ -64,22 +64,24 @@ __global__ void inner_newton(
 
     // float lameps = lam/eps;
     float epslam = eps / lam;
-    float vmin, vmax, lse, g, g_prime;
+    float vmin, vmax;
+    float lse, g, g_prime;
     float err = tol + 1.0f;
 
     int i = 0;
     while (i < n_iter && err > tol)
     {
         i++;
-        // logsumexp for 2 values
+        // logsumexp for 2 values == logaddexp
         // vmin = min(lognu_nJ[idx], -lameps*t[idx]);
         // vmax = max(lognu_nJ[idx], -lameps*t[idx]);
         vmin = min(lognu_nJ[idx], -lam * t[idx] / eps);
         vmax = max(lognu_nJ[idx], -lam * t[idx] / eps);
-        lse = vmax + log(exp(vmin - vmax) + 1.0f);
+        lse = vmax + log1p(exp(vmin - vmax));
 
         // compute g(t[idx]) and g'(t[idx])
         g = lse - t[idx] - lognu[idx] - epslam * logKTu[idx];
+        
         // g_prime = -lameps/(1 + exp(lameps*t[idx] + lognu_nJ[idx])) - 1;
         g_prime = -lam / (eps * (1 + exp(lam * t[idx] / eps + lognu_nJ[idx]))) - 1;
 

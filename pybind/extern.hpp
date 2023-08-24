7a93652d7df59b3d2b2d21b1357e19870d9489f8
Ismael Medina <ismaelmedinasuarez@gmail.com>
2023-08-24 16:31:30 +0200

    Fix return type

--- pybind/extern.hpp
+++ pybind/extern.hpp
@@ -4,4 +4,4 @@ template <typename Dtype>
 at::Tensor LogSumExpCUDA(at::Tensor alpha, int M, Dtype dx);
 
 template <typename Dtype>
-at::Tensor BalanceCUDA(at::Tensor nu_basic, at::Tensor mass_delta, Dtype thresh_step);
+void BalanceCUDA(at::Tensor nu_basic, at::Tensor mass_delta, Dtype thresh_step);

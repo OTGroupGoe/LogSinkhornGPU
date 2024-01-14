c792affce7022ccd1415cd3983041d5f8b5b91d4
Ismael Medina <ismaelmedinasuarez@gmail.com>
2024-01-14 11:02:09 +0100

    Import new functions

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import torch
 
-from LogSinkhornGPUBackend import LogSumExpCUDA_32, LogSumExpCUDA_64
+from LogSinkhornGPUBackend import LogSumExpCUDA_32, LogSumExpCUDA_64, \
+    LogSumExpCUDA_xyeps_32, LogSumExpCUDA_xyeps_64
 
 from .Sinkhorn import *
